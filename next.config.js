083f1a91e828c6bac871527c327e8814ce9304bf
pandayoooo <B10917037@gemail.yuntech.edu.tw>
2023-10-09 01:14:59 +0800

    add enviroment variable

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: { maxid: process.env.maxid , totalPokemon: process.env.totalPokemon },
   images: {
     remotePatterns: [
       {
